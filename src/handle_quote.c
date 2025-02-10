31b03c88dff9bf5c85d1091a0b9d430e353b704d
Dilara_Ulusoy <dilara.akcakoca@gmail.com>
2025-02-10 14:11:23 +0200

    handle quote last changes

--- src/handle_quote.c
+++ src/handle_quote.c
@@ -16,37 +16,64 @@ static int expand_env_variable(const char *line, int *index, char **temp, int *r
 		if (!*temp)
 		{
 			free(env_value);
-			return (-1); // Bellek hatası
+			return (-1);
 		}
 	}
 	ft_memcpy(*temp + *res_index, env_value, env_len);
 	*res_index += env_len;
 	free(env_value);
-	return (1); // Çevre değişkeni başarıyla genişletildi
+	return (1);
 }
 
-static char *process_inner_quotes(const char *line, int *index, int len, char *temp, int *res_index)
+static int extract_quoted_content(const char *line, int *index, char quote, char **temp, int *res_index)
 {
-	char inner_quote;
+    int temp_size = ft_strlen(line) + 1;
 
-	if (*index < len && (line[*index] == '\'' || line[*index] == '"'))
-	{
-		inner_quote = line[*index];
-		(*index)++;
-		while (*index < len && line[*index] != inner_quote)
-		{
-			temp[*res_index] = line[*index];
-			(*res_index)++;
-			(*index)++;
-		}
-		if (*index >= len || line[*index] != inner_quote)
-		{
-			ft_putstr_fd("Error: unclosed inner quote\n", STDERR_FILENO);
-			return (NULL);
-		}
-		(*index)++;
-	}
-	return (temp);
+    while (*index < temp_size && line[*index] != quote)
+    {
+        if (quote == '"' && line[*index] == '$') // Çevre değişkeni genişletme
+        {
+            if (expand_env_variable(line, index, temp, res_index, &temp_size) == -1)
+                return -1;
+            continue;
+        }
+        if (*res_index >= temp_size - 1) // Ensure buffer has enough space
+        {
+            temp_size *= 2;
+            char *new_temp = ft_realloc(*temp, *res_index);
+            if (!new_temp)
+                return -1;
+            *temp = new_temp;
+        }
+        (*temp)[(*res_index)++] = line[(*index)++];
+    }
+    return 0;
+}
+
+static char *handle_nested_quotes(const char *line, int *index, int len, char *temp, int *res_index)
+{
+    char inner_quote;
+
+    while (*index < len && (line[*index] == '\'' || line[*index] == '"'))
+    {
+        inner_quote = line[*index];
+        (*index)++;
+
+        while (*index < len && line[*index] != inner_quote)
+        {
+            temp[*res_index] = line[*index];
+            (*res_index)++;
+            (*index)++;
+        }
+        if (*index >= len || line[*index] != inner_quote) // Error: unclosed inner quote
+        {
+            ft_putstr_fd("Error: unclosed inner quote\n", STDERR_FILENO);
+            free(temp);
+            return NULL;
+        }
+        (*index)++; // Move past the closing inner quote
+    }
+    return temp;
 }
 
 static int check_closing_quote(const char *line, int *index, int len, char *temp, char quote)
@@ -63,46 +90,30 @@ static int check_closing_quote(const char *line, int *index, int len, char *temp
 
 char *process_quoted_content(const char *line, int *index, char quote, int len)
 {
-    int res_index = 0;
-    char *temp;
-    int temp_size = len + 1; // Başlangıçta len + 1 bellek ayırıyoruz.
+	int res_index = 0;
+	char *temp;
 
-    temp = (char *)malloc(temp_size);
+	temp = (char *)malloc(len + 1);
+	if (!temp)
+		return (NULL);
+	(*index)++; // İlk tırnağı atla
+	if (extract_quoted_content(line, index, quote, &temp, &res_index) == -1)
+	{
+		free(temp);
+		return (NULL);
+	}
+	if (check_closing_quote(line, index, len, temp, quote) == -1)
+		return (NULL);
+
+    temp = handle_nested_quotes(line, index, len, temp, &res_index);
     if (!temp)
-        return (NULL);
-    (*index)++; // İlk tırnağı atla
-    while (*index < len && line[*index] != quote)
-    {
-        if (quote == '"' && line[*index] == '$') // Çevre değişkeni genişletme
-        {
-           if (expand_env_variable(line, index, &temp, &res_index, &temp_size) == -1)
-            {
-                free(temp);
-                return (NULL);
-            }
-            continue;
-        }
-        temp[res_index++] = line[*index];
-        (*index)++;
-    }
-    if (check_closing_quote(line, index, len, temp, quote) == -1)
         return NULL;
-    while (*index < len && (line[*index] == '\'' || line[*index] == '"'))
-    {
-        temp = process_inner_quotes(line, index, len, temp, &res_index);
-        if (!temp)
-            return (NULL);
-    }
-    temp[res_index] = '\0';
-    return (temp);
+
+	temp[res_index] = '\0';
+	return (temp);
 }
 
-
-
-
-
 /*
-
 char *process_quoted_content(const char *line, int *index, char quote, int len)
 {
     int res_index = 0;
