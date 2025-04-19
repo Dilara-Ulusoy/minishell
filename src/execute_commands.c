404c7a1c609b2e6a9beab32f4f3cc45327b1b5b8
topahande <ladytastingdata@gmail.com>
2025-04-19 19:02:36 +0300

    Norm fix for execute_commands.c

--- src/execute_commands.c
+++ src/execute_commands.c
@@ -1,96 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute_commands.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htopa <htopa@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/19 17:45:29 by htopa             #+#    #+#             */
+/*   Updated: 2025/04/19 18:57:45 by htopa            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 #include "execution.h"
 
-
-int	execute_commands(t_shell *shell, int num_commands, char ***envp)
+static void	restore_fd(int *original_fd)
 {
-	int				wstatus;
-	int				j;
-	int				exit_code;
-	t_args			*arg_struct;
-	t_cmd_parts		*cmd_parts;
-	int printed_newline;
+	dup2(original_fd[0], STDIN_FILENO);
+	dup2(original_fd[1], STDOUT_FILENO);
+	close(original_fd[0]);
+	close(original_fd[1]);
+}
 
-	if (num_commands == 1)
+static int	if_single_and_builtin(t_shell *shell, int num_commands,
+	char ***envp)
+{
+	t_cmd_parts	*cmd_parts;
+	int			original_fd[2];
+	int			exit_code;
+
+	original_fd[0] = dup(STDIN_FILENO);
+	original_fd[1] = dup(STDOUT_FILENO);
+	cmd_parts = get_command_array(shell->tokens, 1);
+	cmd_parts->num_commands = num_commands;
+	if (is_builtin(cmd_parts))
 	{
-		cmd_parts = get_command_array(shell->tokens, 1);
-		cmd_parts->num_commands = num_commands;
-		if (is_builtin(cmd_parts))
+		exit_code = set_pipe_single_builtin(cmd_parts);
+		if (exit_code != 0)
 		{
-			//int original_stdin = dup(STDIN_FILENO);
-			//int original_stdout = dup(STDOUT_FILENO);
-			int		original_fd[2];
-			original_fd[0] = dup(STDIN_FILENO);
-			original_fd[1] = dup(STDOUT_FILENO);
-			exit_code = set_pipe_single_builtin(cmd_parts);
-			if (exit_code != 0)
-			{
-				//dup2(original_stdin, STDIN_FILENO);
-				//dup2(original_stdout, STDOUT_FILENO);
-				//close(original_stdin);
-				//close(original_stdout);
-				dup2(original_fd[0], STDIN_FILENO);
-				dup2(original_fd[1], STDOUT_FILENO);
-				close(original_fd[0]);
-				close(original_fd[1]);
-				free_cmd_parts(&cmd_parts);
-				cleanup_shell(shell);
-				return (exit_code);
-			}
-			exit_code = run_single_builtin(&cmd_parts, shell, envp, original_fd);
-			dup2(original_fd[0], STDIN_FILENO);
-			dup2(original_fd[1], STDOUT_FILENO);
-			close(original_fd[0]);
-			close(original_fd[1]);
+			restore_fd(original_fd);
 			free_cmd_parts(&cmd_parts);
-			cleanup_shell(shell);
 			return (exit_code);
 		}
+		exit_code = run_single_builtin(&cmd_parts, shell, envp, original_fd);
+		restore_fd(original_fd);
 		free_cmd_parts(&cmd_parts);
+		return (exit_code);
 	}
+	restore_fd(original_fd);
+	free_cmd_parts(&cmd_parts);
+	return (42);
+}
 
-	arg_struct = prepare_struct(num_commands, *envp);
-	if (arg_struct == NULL)
-		exit(EXIT_FAILURE);
+static int	run_command(t_shell *shell, int num_commands,
+	t_args *arg_struct, int j)
+{
+	t_cmd_parts	*cmd_parts;
+	int			exit_code;
 
-	j = -1;
-	while (++j < num_commands)
+	cmd_parts = get_command_array(shell->tokens, j + 1);
+	cmd_parts->num_commands = num_commands;
+	arg_struct->pids[j] = fork();
+	if (arg_struct->pids[j] < 0)
 	{
-		cmd_parts = get_command_array(shell->tokens, j + 1);
-		cmd_parts->num_commands = num_commands;
-
-		arg_struct->pids[j] = fork();
-		if (arg_struct->pids[j] < 0)
-		{
-			free_cmd_parts(&cmd_parts);
-			cleanup_shell(shell);
-			exit(display_error_message(2, arg_struct));
-		}
-		if (arg_struct->pids[j] == 0)
-		{
-			signal(SIGINT, SIG_DFL);
-			signal(SIGQUIT, SIG_DFL);
-
-			exit_code = run_pid(arg_struct, &cmd_parts, shell, is_builtin(cmd_parts));
-			free_cmd_parts(&cmd_parts);
-			cleanup_shell(shell);
-			exit(exit_code);
-		}
-		else
-		{
-			signal(SIGINT, SIG_IGN);
-			signal(SIGQUIT, SIG_IGN);
-		}
-		free_cmd_parts(&cmd_parts);
+		free_cmd_clean_shell(shell, cmd_parts);
+		exit(display_error_message(2, arg_struct));
 	}
+	if (arg_struct->pids[j] == 0)
+	{
+		set_signal_in_child();
+		exit_code = run_pid(arg_struct, &cmd_parts, shell,
+				is_builtin(cmd_parts));
+		free_cmd_clean_shell(shell, cmd_parts);
+		exit(exit_code);
+	}
+	set_signal_in_parent();
+	free_cmd_parts(&cmd_parts);
+	return (0);
+}
 
-	close_and_free(arg_struct, 0);
+static int	wait_commands(int num_commands, t_args *arg_struct)
+{
+	int	printed_newline;
+	int	j;
+	int	wstatus;
+
 	printed_newline = 0;
 	j = -1;
 	while (++j < num_commands)
 	{
 		waitpid(arg_struct->pids[j], &wstatus, 0);
-		if (WIFSIGNALED(wstatus) && WTERMSIG(wstatus) == SIGINT && !printed_newline)
+		if (WIFSIGNALED(wstatus) && WTERMSIG(wstatus) == SIGINT
+			&& !printed_newline)
 		{
 			write(1, "\n", 1);
 			printed_newline = 1;
@@ -104,86 +104,28 @@ int	execute_commands(t_shell *shell, int num_commands, char ***envp)
 		return (128 + WTERMSIG(wstatus));
 	return (EXIT_FAILURE);
 }
-	// // Wait for all child processes
-	// last_exit_code = EXIT_FAILURE;
-	// j = -1;
-	// while (++j < num_commands)
-	// {
-	// 	exited_pid = waitpid(-1, &wstatus, 0);
-	// 	if (exited_pid > 0 && WIFEXITED(wstatus))
-	// 	{
-	// 		exit_code = WEXITSTATUS(wstatus);
-	// 		k = -1;
-	// 		while (++k < num_commands)
-	// 		{
-	// 			if (arg_struct->pids[k] == exited_pid)
-	// 			{
-	// 				if (used_execve[k] == 1)
-	// 					last_exit_code = exit_code;
-	// 				else
-	// 				{
-	// 					if (arg_struct->pids != NULL)
-	// 					{
-	// 						free(arg_struct->pids);
-	// 						arg_struct->pids = NULL;
-	// 					}
-	// 					if (arg_struct != NULL)
-	// 					{
-	// 						free(arg_struct);
-	// 						arg_struct = NULL;
-	// 					}
-	// 					exit(exit_code); // Exit parent
-	// 				}
-	// 				break ;
-	// 			}
-	// 		}
-	// 	}
-	// }
-	// // Cleanup and return last exit code
-	// if (arg_struct->pids != NULL)
-	// {
-	// 	free(arg_struct->pids);
-	// 	arg_struct->pids = NULL;
-	// }
-	// if (arg_struct != NULL)
-	// {
-	// 	free(arg_struct);
-	// 	arg_struct = NULL;
-	// }
-	// return (last_exit_code);
 
+int	execute_commands(t_shell *shell, int num_commands, char ***envp)
+{
+	int		exit_code;
+	t_args	*arg_struct;
+	int		j;
 
-
-// int	execute_commands(t_shell *shell, int num_commands, char **envp)
-// {
-//  	int		wstatus;
-//  	int		j;
-//  	t_args	*arg_struct;
-//  	t_cmd_parts *cmd_parts;
-
-// 	//printf("num_commands: %d\n", num_commands);
-// 	arg_struct = prepare_struct(num_commands, envp);
-// 	if (arg_struct == NULL)
-// 		exit(EXIT_FAILURE);
-// 	j = -1;
-// 	while (++j < num_commands)
-// 	{
-// 		cmd_parts = get_command_array(shell->tokens, j + 1);
-// 		cmd_parts->num_commands = num_commands;
-// 		arg_struct->pids[j] = fork();
-// 		if (arg_struct->pids[j] < 0)
-// 			return (display_error_message(2, arg_struct)); // exit
-// 		if (arg_struct->pids[j] == 0)
-// 			run_pid(arg_struct, cmd_parts, shell, is_builtin(cmd_parts));
-// 		free(cmd_parts);
-// 	}
-// 	close_and_free(arg_struct, 0);
-// 	j = -1;
-// 	while (++j < num_commands)
-// 		waitpid(arg_struct->pids[j], &wstatus, 0);
-// 	free(arg_struct->pids);
-// 	free(arg_struct);
-// 	if (WIFEXITED(wstatus))
-// 		return (WEXITSTATUS(wstatus)); //exit
-// 	return (EXIT_FAILURE); //exit
-// }
+	if (num_commands == 1)
+	{
+		exit_code = if_single_and_builtin(shell, num_commands, envp);
+		if (exit_code != 42)
+		{
+			cleanup_shell(shell);
+			return (exit_code);
+		}
+	}
+	arg_struct = prepare_struct(num_commands, *envp);
+	if (arg_struct == NULL)
+		exit(EXIT_FAILURE);
+	j = -1;
+	while (++j < num_commands)
+		run_command(shell, num_commands, arg_struct, j);
+	close_and_free(arg_struct, 0);
+	return (wait_commands(num_commands, arg_struct));
+}
