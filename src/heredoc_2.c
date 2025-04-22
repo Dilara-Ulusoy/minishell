cc11ea5ed4e3a2bb94fe679f2cad5f224a881cef
Dilara Akcakoca <dakcakoc@c2r5p5.hive.fi>
2025-04-22 19:08:51 +0300

    heredoc exit code fixed

--- src/heredoc_2.c
+++ src/heredoc_2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc_2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: htopa <htopa@student.hive.fi>              +#+  +:+       +#+        */
+/*   By: dakcakoc <dakcakoc@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 15:35:01 by dakcakoc          #+#    #+#             */
-/*   Updated: 2025/04/22 17:11:32 by htopa            ###   ########.fr       */
+/*   Updated: 2025/04/22 19:04:55 by dakcakoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,14 +44,16 @@ static int	heredoc_loop(int tmp_fd, const char *delimiter,
 	while (1)
 	{
 		line = get_input("> ");
-		if (g_signal == SIGINT)
+		if (g_signal == SIGINT || !line)
 		{
+			if(!line)
+				fprintf(stderr, "warning: heredoc delimited by end-of-file (wanted `%s')\n", delimiter);
 			free(line);
 			close(tmp_fd);
 			unlink(path);
 			return (-1);
 		}
-		if (!line || ft_strncmp(line, delimiter, ft_strlen(delimiter)) == 0)
+		if (ft_strncmp(line, delimiter, ft_strlen(delimiter)) == 0)
 		{
 			free(line);
 			break ;
